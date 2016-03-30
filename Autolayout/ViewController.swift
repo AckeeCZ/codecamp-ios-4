3233fb7f16024db247f3c74115d33b7aecdc8977
Jan Mísař <misar.jan@gmail.com>
2016-03-30 15:47:14 +0200

    New project.

---
+++ Autolayout/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Autolayout
+//
+//  Created by Jan Mísař on 30.03.16.
+//  Copyright © 2016 Ackee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
