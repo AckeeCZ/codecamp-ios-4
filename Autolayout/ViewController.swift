4d33f86005fecd3fba6952bbff6b73621171427a
Jan Mísař <misar.jan@gmail.com>
2016-03-30 18:14:50 +0200

    StackView example.

--- Autolayout/ViewController.swift
+++ Autolayout/ViewController.swift
@@ -20,49 +20,62 @@ class ViewController: UIViewController {
         label.numberOfLines = 0
         label.text = "Lorem ipsum dolor sit amet"
         //label.text = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-        self.view.addSubview(label)
-        
-        label.snp_makeConstraints { make in
-            make.leading.equalTo(20)
-            make.trailing.equalTo(-20)
-            make.top.equalTo(0)
-        }
+//        self.view.addSubview(label)
+//        label.snp_makeConstraints { make in
+//            make.leading.equalTo(20)
+//            make.trailing.equalTo(-20)
+//            make.top.equalTo(0)
+//        }
         
         let redView = UIView()
         redView.backgroundColor = .redColor()
-        self.view.addSubview(redView)
-        redView.snp_makeConstraints { make in
-            make.leading.equalTo(label.snp_leading)
-            make.top.equalTo(label.snp_bottom)
-            make.height.equalTo(100)
-        }
+//        self.view.addSubview(redView)
+//        redView.snp_makeConstraints { make in
+//            make.leading.equalTo(label.snp_leading)
+//            make.top.equalTo(label.snp_bottom)
+//            make.height.equalTo(100)
+//        }
         
         let blueView = UIView()
         blueView.backgroundColor = .blueColor()
-        self.view.addSubview(blueView)
-        blueView.snp_makeConstraints { make in
-            make.leading.equalTo(redView.snp_trailing)
-            make.trailing.equalTo(label.snp_trailing)
-            make.top.equalTo(redView.snp_top)
-            make.height.equalTo(redView.snp_height)
-            make.width.equalTo(redView.snp_width)
-        }
+//        self.view.addSubview(blueView)
+//        blueView.snp_makeConstraints { make in
+//            make.leading.equalTo(redView.snp_trailing)
+//            make.trailing.equalTo(label.snp_trailing)
+//            make.top.equalTo(redView.snp_top)
+//            make.height.equalTo(redView.snp_height)
+//            make.width.equalTo(redView.snp_width)
+//        }
         
         let profileImage = UIImage(named: "avatar")
         let profilePictureView = UIImageView(image: profileImage)
-        self.view.addSubview(profilePictureView)
-        profilePictureView.snp_makeConstraints { make in
-            make.leading.equalTo(20)
-            make.top.equalTo(redView.snp_bottom).offset(20)
-        }
-        profilePictureView.setContentHuggingPriority(UILayoutPriorityRequired, forAxis: .Horizontal)
+//        self.view.addSubview(profilePictureView)
+//        profilePictureView.snp_makeConstraints { make in
+//            make.leading.equalTo(20)
+//            make.top.equalTo(redView.snp_bottom).offset(20)
+//        }
+//        profilePictureView.setContentHuggingPriority(UILayoutPriorityRequired, forAxis: .Horizontal)
         
         let titleLabel = UILabel()
         titleLabel.text = "Jan Mísař"
-        self.view.addSubview(titleLabel)
-        titleLabel.snp_makeConstraints { make in
-            make.leading.equalTo(profilePictureView.snp_trailing).offset(20)
-            make.centerY.equalTo(profilePictureView)
+//        self.view.addSubview(titleLabel)
+//        titleLabel.snp_makeConstraints { make in
+//            make.leading.equalTo(profilePictureView.snp_trailing).offset(20)
+//            make.centerY.equalTo(profilePictureView)
+//            make.trailing.equalTo(-20)
+//        }
+        
+        let redBlueStackView = UIStackView(arrangedSubviews: [redView, blueView])
+        redBlueStackView.axis = .Horizontal
+        
+        let pictureNameStackView = UIStackView(arrangedSubviews: [profilePictureView, titleLabel])
+        pictureNameStackView.axis = .Horizontal
+        
+        let verticalStackView = UIStackView(arrangedSubviews: [label, redBlueStackView, pictureNameStackView])
+        verticalStackView.axis = .Vertical
+        self.view.addSubview(verticalStackView)
+        verticalStackView.snp_makeConstraints { make in
+            make.leading.top.equalTo(20)
             make.trailing.equalTo(-20)
         }
         
